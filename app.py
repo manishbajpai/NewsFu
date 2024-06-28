c807a46f87faaf24ef86e05cce5fcdf2482a4c45
Manish <manish.baj@gmail.com>
2024-06-28 13:31:06 -0700

    Show Kron4 news in html

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from datetime import datetime
 
 app = Flask(__name__)
 
-def get_articles_by_category(query_date):
+def get_cnn_by_category(query_date, categories):
     base_url = "https://lite.cnn.com/"  # Set the base URL
     conn = sqlite3.connect('example.db')
     c = conn.cursor()
@@ -14,7 +14,6 @@ def get_articles_by_category(query_date):
     articles = c.fetchall()
     
     # Organize articles by category
-    categories = {}
     for article in articles:
         cat = article[3].capitalize()
         if cat not in categories:
@@ -32,11 +31,44 @@ def get_articles_by_category(query_date):
     conn.close()
     return categories
 
+def get_kron4_by_category(query_date, categories):
+    base_url = "https://www.kron4.com/"  # Set the base URL
+    conn = sqlite3.connect('example.db')
+    c = conn.cursor()
+    
+    # Query to fetch all articles grouped by category
+    c.execute("SELECT date, link, title, category FROM kron4 WHERE date LIKE ? ORDER BY category", (f"{query_date}%",))
+    articles = c.fetchall()
+    
+    # Organize articles by category
+    for article in articles:
+        cat = article[3].capitalize()
+        if cat not in categories:
+            categories[cat] = []
+
+        link = article[1]
+
+        updated = (article[0], article[1], article[2], cat)
+        categories[cat].append(updated)
+    
+    # Close the database connection
+    conn.close()
+    return categories
+
 @app.route('/', methods=['GET'])
 def home():
     date_query = request.args.get('date', default=datetime.now().strftime('%Y-%m-%d'))
-    categories = get_articles_by_category(date_query)
+    categories = {}
+
+    get_cnn_by_category(date_query, categories)
+    get_kron4_by_category(date_query, categories)
+
     return render_template('index.html', date=date_query, categories=categories)
 
+@app.route('/settings')
+def settings():
+    # Logic to render settings page
+    return render_template('settings.html')
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host="0.0.0.0", port=5001)
